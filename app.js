50babdfa4dce1b1cd68ecbbd8a49cb07bf0d4ab9
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-12-12 23:52:37 +0530

    Remove unnecessary dependancies from app.js

--- app.js
+++ app.js
@@ -2,55 +2,28 @@ const express               = require("express"),
       app                   = express(),
       bodyparser            = require("body-parser"),
       mongoose              = require("mongoose"),
-      flash                 = require("connect-flash");
-      passport              = require("passport"),
-      LocalStrategy         = require("passport-local"),
-      FacebookStrategy      = require('passport-facebook').Strategy;
-      GoogleStrategy        = require('passport-google-oauth20').Strategy;
-      passportLocalMongoose = require("passport-local-mongoose"),
-      expressSession        = require("express-session"),
-      nodemailer            = require("nodemailer"),
-      async                 = require("async"),
       methodOverride        = require("method-override"),
-      Campground            = require("./models/campground"),
-      Comments              = require("./models/comments"),
-      User                  = require("./models/user"),
+      middleware            = require("./middleware/index");
       seed                  = require("./seed");
 
-    require("dotenv").config();
+require("dotenv").config();
 
     //ROUTES
-var campgroundRoute = require("./routes/campgrounds"),
-    commentRoute    = require("./routes/comments"),
-    indexRoute      = require("./routes/index"),
-    userRoute       = require("./routes/user"),
-    notificationRoute=require("./routes/notification");
+const campgroundRoute = require("./routes/campgrounds"),
+      commentRoute    = require("./routes/comments"),
+      indexRoute      = require("./routes/index"),
+      userRoute       = require("./routes/user"),
+      notificationRoute=require("./routes/notification");
 
 mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true});
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
 app.use(bodyparser.urlencoded({extended:true}));
-app.use(express.static(__dirname+"/public"));
 app.set("view engine","ejs");
 app.use(methodOverride("_method"));
-app.use(flash());
 //seed();
-
-app.use(async function(req, res, next){
-   res.locals.currentUser = req.user;
-   if(req.user) {
-    try {
-      let user = await User.findById(req.user._id).populate('notifications', null, { isRead: false }).exec();
-      res.locals.notifications = user.notifications.reverse();
-    } catch(err) {
-      console.log(err.message);
-    }
-   }
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
-});
+middleware(app);
 
 app.use("/campground",campgroundRoute);
 app.use("/campground/:id/comments",commentRoute);
