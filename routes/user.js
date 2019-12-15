4a70b7ab5fe2d6dab1334b0768e7d690f94a2134
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-12-15 18:59:59 +0530

    Add user route

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express     = require("express");
+      router      = express.Router();
+      User        = require("../models/user");
+      Campground  = require("../models/campground");
+
+// user profile
+router.get('/:id', async function(req, res) {
+    try {
+      let user = await User.findById(req.params.id).exec();
+      let campgrounds= await Campground.find().where("author.id").equals(user._id).exec();
+      res.render('user/show', { user,campgrounds });
+    } 
+    catch(err) {
+      req.flash('error', err.message);
+      return res.redirect('back');
+    }
+  });
+
+module.exports=router;
