d7effe160cd049478965e3084bfb69eb2b1ac6e2
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-04-22 04:14:34 +0530

    Remove  forgotPassword.js

--- models/notification.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var notificationSchema = new mongoose.Schema({
-	username: String,
-	campgroundId: String,
-	isRead: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model("Notification", notificationSchema);
